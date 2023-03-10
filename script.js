ada7b85125331164b4802474407527c5cc5ff97d
Finn <119108081+elefinnt@users.noreply.github.com>
2023-03-10 17:01:45 +1300

    Add files via upload
    cleaned up code and removed code to refactor into more effective code

--- script.js
+++ script.js
@@ -13,7 +13,7 @@
 //sr
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20; //needs to be let because we want to adjust it
-//
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -29,24 +29,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').textContent = secretNumber; //links the .number content to the secretNumber cost above
 
     document.querySelector('body').style.backgroundColor = '#60b347';
-
     document.querySelector('.number').style.width = '30ren';
-
-    //when guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('.score').textContent = 0;
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
     }
-
-    //when guess is too low
-  } else if (guess < secretNumber) {
+    //when guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low!';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high!' : 'Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
@@ -54,6 +46,28 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+
+  //   else if (guess > secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too high!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+
+  //   //when guess is too low
+  // } else if (guess < secretNumber) {
+  //   if (score > 1) {
+  //     document.querySelector('.message').textContent = 'Too Low!';
+  //     score--;
+  //     document.querySelector('.score').textContent = score;
+  //   } else {
+  //     document.querySelector('.message').textContent = 'You lost the game';
+  //     document.querySelector('.score').textContent = 0;
+  //   }
+  // }
 });
 
 //reset switch build
